12715537e82305d8a8c8ff49477dfbc9f89646b1
penguin0731 <276087919@qq.com>
2022-10-19 10:19:33 +0800

    更新代码

---
+++ React/官方脚手架/pure-component/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import TaskContainer from './component/TaskContainer'
+
+export default class App extends Component {
+  render() {
+    return (
+      <TaskContainer />
+    )
+  }
+}
