63735da934ee954ce5e920a13d38b866466cbe69
penguin0731 <276087919@qq.com>
2022-10-25 17:54:41 +0800

    更新代码

---
+++ React/官方脚手架/cross-cutting/src/App.js
@@ -0,0 +1,13 @@
+// import Test from "./Test";
+import TestRenderProps from "./TestRenderProps";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Test /> */}
+      <TestRenderProps />
+    </div>
+  );
+}
+
+export default App;
